8302479a02d832c061235878df2b6cc1bf92f286
Rewdawrath <132688148+Rewdawrath@users.noreply.github.com>
2023-05-26 04:37:20 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@OOPIntro-RYUICHISEKIGUCH/student.h
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class student{
+private:
+  long id;
+  string name;
+  int age;
+
+public:
+  void set_name(string="Yme");
+  void print_name();
+  void set_age(int);
+  void get_age();
+
+};
+
+void student::set_name(string n){
+  name=n;
+  
+}
+
+void student::print_name(){
+  cout<<"Name: "<<name<<endl;
+}
+
+void student::set_age(int n){
+  age=n;
+}
+
+void student::get_age(){
+  return age;
+}
